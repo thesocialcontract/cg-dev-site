39bd0654be8472d772106539bccbce7ac00f554c
Caleb George <1477500+thesocialcontract@users.noreply.github.com>
2021-12-08 18:18:47 -0800

    simplify site

---
+++ cgsite/static/js/main.js
@@ -0,0 +1,4 @@
+particlesJS.load('particles-js', 'static/assets/particles-index.json', function() {
+    console.log('callback - particles.js config loaded');
+    document.getElementById('particles-js').classList.add('fixed');
+  });
