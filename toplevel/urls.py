d31ac31746465454749d4498e92a8ff7b15b1b35
Caleb George <1477500+thesocialcontract@users.noreply.github.com>
2022-03-24 12:20:50 -0700

    add 404/500. Fix footer

--- toplevel/urls.py
+++ toplevel/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('404/', views.http404, name='http404'),
+    path('500/', views.http500, name='http500'),
 ]
--- toplevel/views.py
+++ toplevel/views.py
@@ -6,3 +6,13 @@ def index(request):
     template = loader.get_template("toplevel/index.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+def http404(request):
+    template = loader.get_template("404.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def http500(request):
+    template = loader.get_template("500.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
