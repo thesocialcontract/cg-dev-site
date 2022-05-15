70730d9bf55eeb214918fc9b72dd5b58a6e23a0c
Caleb George <1477500+thesocialcontract@users.noreply.github.com>
2022-05-15 11:50:06 -0700

    Added tagging, renamed blogpage to content page

--- cms/models.py
+++ cms/models.py
@@ -1,35 +1,19 @@
 from django.db import models
-
-from wagtail.core.models import Page
+from wagtail.core.models import Page # Page allows you to assign models into the wagtail tree structure
 from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.search import index
 from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
-
-class BlogPageTag(TaggedItemBase):
-    """ See: https://docs.wagtail.org/en/stable/reference/pages/model_recipes.html#tagging """
-    content_object = ParentalKey('cms.BlogPage', on_delete=models.CASCADE, related_name='tagged_items')
-
+from cms.blocks import CodeBlock
 
 # Goals:
-# - Display fields on template page
 # - Get Blog Page Index fields
 # - Display list of blog pages on blog index
-# - Style Blog Page
 # - Style Blog Index Page
-
-# Ok. So.
-# Wagtail is a CMS, so the idea with wagtail is that we create content, and manage it via wagtail.
-# Which means, any sort of page that you want to integrate into an interface means you need to build a page
-# (any sort of webpage with custom content) through wagtail's interfacing. That's here in this page.
-# 
-# So wagtail's routing works by creating a tree structure of pages. You assign a page as the root page
-# (analogus to assigning a page to "/") and then assign children to it. Children will inform wagtail what the
-# url structure ought be. In other words, assinging the page "Foobar" with a slug "bar" as a child to "root page"
-# as the root page, then you can access foobar by going to "domain.com/bar" and access root page by going to "domain.com/"
 
 class BlogIndexPage(Page):
     """
@@ -42,31 +26,38 @@ class BlogIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('introduction', classname="full"),
     ]
-    subpage_types = ['BlogPage'] # Only BlogPages can live under this.
+    subpage_types = ['ContentPage']
 
-class BlogPage(Page):
+
+class ContentTag(TaggedItemBase):
+    """ See: https://docs.wagtail.org/en/stable/reference/pages/model_recipes.html#tagging """
+    content_object = ParentalKey('cms.ContentPage', on_delete=models.CASCADE, related_name='tagged_content')
+
+class ContentPage(Page):
     """
-    A Blog Page used for basic article content.
+    A Content Page used for basic article content.
     """
-    # DB Fields
-    author = models.CharField(max_length=255)
+    tags = ClusterTaggableManager(through=ContentTag, blank=True)
     date = models.DateField("Post date")
-
     body = StreamField([
-        ('heading', blocks.CharBlock(form_classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
+
+        ('richtext', blocks.RichTextBlock(features=[
+                    'h3','h4','h5','h6','bold','italic','ol',
+                    'ul','hr','link','document-link', 'embed',
+                    'superscript','subscript','strikethrough','blockquote'])),
+        ('code', CodeBlock()),
         ('image', ImageChooserBlock()),
     ])
 
     # Search index Configuration
     search_fields = Page.search_fields + [
-        #index.SearchField('body'),
+        index.SearchField('body'),
         index.FilterField('date'),
     ]
 
     # Editor Panels Configuration
     content_panels = Page.content_panels + [
-        FieldPanel('author'),
         FieldPanel('date'),
         StreamFieldPanel('body'),
+        FieldPanel('tags'),
     ]
--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+# Notes on Wagtail Models:
+# So wagtail's routing works by creating a tree structure of wagtail pages. You assign a page as the root page
+# (analogus to assigning a page to "/") and then assign children to it. Children will inform wagtail what the
+# url structure ought be. In other words, taking the wagtail page "Foobar" with a slug "bar", and assigning it 
+# as a child  to "root page" as the root page, then you can access foobar by going to "domain.com/bar" and 
+# access root page by going to "domain.com/"
+#
+# You can create a wagtail page by making a page model that inherits from wagtail's page model.
